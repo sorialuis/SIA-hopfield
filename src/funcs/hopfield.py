ee7d8848f5ce0a3389c01a93bc32b6f2aebfd20b
Luis <lsoria@wilab.io>
2023-02-21 15:48:56 -0300

    delete commented code

--- src/funcs/hopfield.py
+++ src/funcs/hopfield.py
@@ -6,9 +6,6 @@ def learn(pw, ph, patterns):
     identity = np.identity(pw * ph)
     generalWeightMatrix = np.zeros((pw * ph, pw * ph))
 
-    # identity = np.identity(max(pw, ph))
-    # generalWeightMatrix = np.zeros((max(pw, ph), max(pw, ph)))
-
     for i in range(len(patterns)):
         generalWeightMatrix += np.dot(patterns[i].reshape(pw * ph, 1), patterns[i].reshape(1,pw * ph)) - identity
     return generalWeightMatrix
